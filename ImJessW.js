f47fd22a425b562e85355e9102ee95c8f32708e0
Intern Jess <internjess@Michele-Muses-Computer.local>
2013-10-25 10:46:46 -0600

    I added the logic for forms.

--- ImJessW.js
+++ ImJessW.js
@@ -1,22 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-$("#QButton").on("click", function(){
-	// console.log("CLICKED!!!!!")
-	$("#QButton").hide();
-	var forms= $(".form-group");
-	var submitButton= $("#bottomButton");
-	forms.show().focus();
-	submitButton.show();
-})
-$("#bottomButton").on("click", function(){
-	var forms= $(".form-group");
-	var submitButton= $("#bottomButton");
-	var QButton=$("#QButton");
-	forms.hide();
-	submitButton.hide();
-	QButton.show();
+	$("#QButton").on("click", function(){
+		// console.log("CLICKED!!!!!")
+		$("#QButton").hide();
+		var forms= $(".form-group");
+		var submitButton= $("#bottomButton");
+		var nameForm=$("#first")
 
-})
+		forms.show();
+		nameForm.focus(); //sets cursor to first form field
+		submitButton.show();
+	});
 
 
-});
+	$("#formInfo").on("submit", function(){
+		console.log("you are a Javascript genius... kind of")
+		var forms= $(".form-group");
+		var submitButton= $("#formInfo");
+		var QButton= $("#QButton");
+		var first= $("#first");
+		var lastName= $("#lastName");
+		var email= $("#email");
+		var message= $("#message");
+		var badInfo= $("#badInfo")
+	// dialogbox
+
+
+	// logic for forms left blank
+
+		if (first.val() === "" || lastName.val()=== "" 
+			|| email.val() === "" || message.val() === "" ) {
+			
+			badInfo.show();
+			
+		}
+		
+		else{
+			alert("Thank you. I look forward to speaking with you soon!")
+			forms.hide(); //to hide form
+			forms.val("");  //to clear form
+			submitButton.hide(); //hide submit button
+			QButton.show(); //show question button again
+			console.log("the end")
+		}	
+		return false
+	});
+});		
+
+
+
+
+
+
